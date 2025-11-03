43d9cac83eccd6d3a1cec0fe69af5b01feb148de
YugrajaSamadhiya <yugrajasam@gmail.com>
2025-11-03 19:46:08 +0530

    new commit

---
+++ Day21A_swap the first and last digit of a number.c
@@ -0,0 +1,14 @@
+//Write a program to swap the first and last digit of a number.
+#include <stdio.h>
+#include <math.h>
+int main() {
+    int n, first, last, digits, swapped;
+    printf("Enter a number: ");
+    scanf("%d", &n);
+    last = n % 10;
+    digits = (int)log10(n);
+    first = n / pow(10, digits);
+    swapped = last * pow(10, digits) + (n % (int)pow(10, digits)) / 10 * 10 + first;
+    printf("Number after swapping first and last digits: %d\n", swapped);
+    return 0;
+}
