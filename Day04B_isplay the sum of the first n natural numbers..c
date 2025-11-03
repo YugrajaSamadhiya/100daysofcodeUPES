b4a7284323896d48bb8d2582334891f3c6e62d3e
YugrajaSamadhiya <yugrajasam@gmail.com>
2025-11-03 19:00:02 +0530

    new commit

--- Day04B_isplay the sum of the first n natural numbers..c
+++ Day04B_isplay the sum of the first n natural numbers..c
@@ -1,1 +1,13 @@
-q
+//Write a program to find and display the sum of the first n natural numbers.
+#include <stdio.h>
+int main() {
+    int n, i = 1, sum = 0;
+    printf("Enter n: ");
+    scanf("%d", &n);
+    while(i <= n) {
+        sum += i;
+        i++;
+    }
+    printf("Sum of first %d natural numbers is: %d\n", n, sum);
+    return 0;
+}
