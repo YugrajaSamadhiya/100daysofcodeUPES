585d689f70e65d31aaee576337aaa5acd4e0d383
YugrajaSamadhiya <yugrajasam@gmail.com>
2025-11-03 19:51:53 +0530

    new commit

---
+++ Day22B_sum of the series.c
@@ -0,0 +1,12 @@
+//Write a program to find the sum of the series: 1 + 3/4 + 5/6 + 7/8 + … up to n terms.
+#include <stdio.h>
+int main() {
+    int n, i;
+    float sum = 0.0;
+    printf("Enter number of terms: ");
+    scanf("%d", &n);
+    for(i = 1; i <= n; i++)
+        sum += (float)(2 * i - 1) / (2 * i);
+    printf("Sum of the series = %.4f\n", sum);
+    return 0;
+}
